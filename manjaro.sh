e3865fd29362c37fb370c6a8751957e7f497035c
Inom T <i@inomoz.ru>
2020-07-25 11:34:11 +0300

    add tmux

--- manjaro.sh
+++ manjaro.sh
@@ -6,6 +6,6 @@ pamac upgrade -a
 
 mkdir -p ~/downloads/video ~/.local/bin
 
-pamac install --no-confirm bash-completion neovim rtorrent 
+pamac install --no-confirm bash-completion tmux neovim rtorrent 
 
 pamac install --no-confirm xscreensaver
