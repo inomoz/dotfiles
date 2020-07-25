1b03819cfce28a2ceacfdd410f75c0d8d7376aa6
Inom T <i@inomoz.ru>
2020-07-25 11:47:44 +0300

    adjust install script

--- manjaro.sh
+++ manjaro.sh
@@ -6,9 +6,11 @@ pamac upgrade -a
 
 mkdir -p ~/downloads/video ~/.local/bin
 
-pamac install --no-confirm bash-completion tmux neovim rtorrent git
+pamac install --no-confirm bash-completion tmux neovim rtorrent git tmux inkscape telegram-desktop pavucontrol discord
 
 pamac install --no-confirm xscreensaver
 
+# pamac install --no-confirm chromium-vaapi
+
 # tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2> /dev/null
