23999d7be61bf86e6f6fe3d49115dce576838943
Inom T <i@inomoz.ru>
2020-07-25 14:06:44 +0300

    additional packages & i3 config

--- manjaro.sh
+++ manjaro.sh
@@ -4,13 +4,16 @@
 # Update system
 pamac upgrade -a
 
-mkdir -p ~/downloads/video ~/.local/bin
+mkdir -p ~/downloads/video ~/pictures/screenshots ~/.local/bin
 
-pamac install --no-confirm bash-completion rofi tmux neovim rtorrent git tmux inkscape telegram-desktop pavucontrol discord
+pamac install --no-confirm bash-completion xclip maim rofi tmux neovim rtorrent git tmux inkscape telegram-desktop pavucontrol discord
 
 pamac install --no-confirm xscreensaver
 
-# pamac install nerd-fonts-iosevka chromium-vaapi
+# pamac install nerd-fonts-iosevka chromium-vaapi spotif pycharm-professional
 
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2> /dev/null
+sudo gpasswd -a inom docker
+sudo systemctl enable docker
+
