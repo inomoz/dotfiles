4a6e8fae75d429289511bfbca01b434f3af3d86f
Inom T <i@inomoz.ru>
2020-07-25 11:38:16 +0300

    add tmux plugin manager

--- manjaro.sh
+++ manjaro.sh
@@ -6,6 +6,9 @@ pamac upgrade -a
 
 mkdir -p ~/downloads/video ~/.local/bin
 
-pamac install --no-confirm bash-completion tmux neovim rtorrent 
+pamac install --no-confirm bash-completion tmux neovim rtorrent git
 
 pamac install --no-confirm xscreensaver
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
