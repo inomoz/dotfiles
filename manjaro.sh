55bc5ea5088906b1f859ebe892a98c4f6cb28c27
Inom T <i@inomoz.ru>
2020-07-25 14:23:46 +0300

    add additional stuff, binaries, config

--- manjaro.sh
+++ manjaro.sh
@@ -6,11 +6,11 @@ pamac upgrade -a
 
 mkdir -p ~/downloads/video ~/pictures/screenshots ~/.local/bin
 
-pamac install --no-confirm bash-completion xclip maim rofi tmux neovim rtorrent git tmux inkscape telegram-desktop pavucontrol discord
+pamac install --no-confirm bash-completion jq xclip maim rofi tmux neovim rtorrent git tmux gimp inkscape telegram-desktop pavucontrol discord
 
 pamac install --no-confirm xscreensaver
 
-# pamac install nerd-fonts-iosevka chromium-vaapi spotif pycharm-professional
+# pamac install nerd-fonts-iosevka chromium-vaapi spotif pycharm-professional i3-resurrect
 
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2> /dev/null
