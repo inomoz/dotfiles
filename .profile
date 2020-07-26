dd9c2302f3f0339c577a38ffa2b61125f855c2ec
Inom T <i@inomoz.ru>
2020-07-26 19:25:44 +0300

    Various adjustements & new config

--- .profile
+++ .profile
@@ -1,4 +1,3 @@
-export QT_QPA_PLATFORMTHEME="qt5ct"
 export EDITOR=/usr/bin/nvim
 export PAGER=less
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
@@ -6,3 +5,12 @@ export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 export BROWSER=/usr/bin/chromium
 EXTRA_BINARY_PATH=$HOME/.local/bin
 PATH=$EXTRA_BINARY_PATH:$PATH
+
+# hidpi for gtk apps
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
+export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_STYLE_OVERRIDE=kvantum
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export "QT_SCREEN_SCALE_FACTORS=2;2"
+
--- manjaro.sh
+++ manjaro.sh
@@ -6,13 +6,13 @@ pamac upgrade -a
 
 mkdir -p ~/downloads/video ~/pictures/screenshots ~/.local/bin
 
-pamac install --no-confirm bash-completion fcitx fcitx-configtool barrier jq xclip maim rofi tmux neovim rtorrent git tmux gimp inkscape telegram-desktop pavucontrol discord steam blender openscad neomutt isync msmtp pass lynx indent perl-anyevent-i3 obs-studio mpv
+pamac install --no-confirm bash-completion fcitx fcitx-configtool barrier jq xclip maim rofi tmux neovim rtorrent git tmux gimp inkscape telegram-desktop pavucontrol discord steam blender openscad neomutt isync msmtp pass lynx indent perl-anyevent-i3 obs-studio mpv qownnotes translate-shell feh keynav task anki
 pamac install --no-confirm xscreensaver
 
-# pamac install nerd-fonts-iosevka chromium-vaapi spotif pycharm-professional i3-resurrect anydesk-bin mutt-wizard-git
-
+# pamac install nerd-fonts-iosevka chromium-vaapi spotif pycharm-professional i3-resurrect anydesk-bin mutt-wizard-git 
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2> /dev/null
 sudo gpasswd -a inom docker
 sudo systemctl enable docker
+
 
