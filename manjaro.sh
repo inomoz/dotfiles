38457792aac4dc0d95918c1bd21aed5fbe468e7a
Inom T <i@inomoz.ru>
2020-07-27 11:09:54 +0300

    add bash configuration

--- manjaro.sh
+++ manjaro.sh
@@ -6,7 +6,7 @@ pamac upgrade -a
 
 mkdir -p ~/downloads/video ~/pictures/screenshots ~/.local/bin
 
-pamac install --no-confirm bash-completion fcitx fcitx-configtool barrier jq xclip maim rofi tmux neovim rtorrent git tmux gimp inkscape telegram-desktop pavucontrol discord steam blender openscad neomutt isync msmtp pass lynx indent perl-anyevent-i3 obs-studio mpv qownnotes translate-shell sxiv keynav task anki alacritty
+pamac install --no-confirm bash-completion fcitx fcitx-configtool barrier jq xclip maim rofi tmux neovim rtorrent git tmux gimp inkscape telegram-desktop pavucontrol discord steam blender openscad neomutt isync msmtp pass lynx indent perl-anyevent-i3 obs-studio mpv qownnotes translate-shell sxiv keynav task anki alacritty fzf
 pamac install --no-confirm xscreensaver
 
 # pamac install nerd-fonts-iosevka chromium-vaapi spotif pycharm-professional i3-resurrect anydesk-bin mutt-wizard-git 
