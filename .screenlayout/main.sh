11caca0ca45a3df9210631d1bd12acba983eddb2
Inom T <i@inomoz.ru>
2021-03-04 14:52:34 +0300

    fixes

--- .screenlayout/main.sh
+++ .screenlayout/main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 3840x240 --rotate right --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x1680 --rotate normal --output HDMI-A-0 --mode 3840x2160 --pos 3840x0 --rotate left --output DVI-D-0 --off
